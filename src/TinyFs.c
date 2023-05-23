28cdfd9d05ed675a8a1808b04c4be308f7bbac61
Oleksandr_Gersymchuk <ger_1987@ukr.net>
2023-05-23 10:18:11 +0300

    Temp

--- src/TinyFs.c
+++ src/TinyFs.c
@@ -1,10 +1,32 @@
 ﻿#include <stdint.h>
 #include <stdio.h>
 
-#include "Fs.h"
+#include "TinyFs.h"
 
 /*
  * FAT entry description
+ *
+ * File begin:
+ * - Type (1 byte)
+ * - File name (2 bytes)
+ * - File size (2 bytes)
+ * - Next sector address (2 bytes)
+ *
+ * File next:
+ * - Type (1 byte)
+ * - Next sector address (2 bytes)
+ * - First sector address (2 bytes)
+ *
+ * File last:
+ * - Type (1 byte)
+ * - Bytes per sector (2 bytes)
+ * - First sector address (2 bytes)
+ *
+ * File alone:
+ * - Type (1 byte)
+ * - File name (2 bytes)
+ * - File size (2 bytes)
+ * 
  * +------+-----------------------------------+
  * | size |   description                     |
  * +------+-----------------------------------+
@@ -22,35 +44,35 @@
  * +------+-----------------------------------+
  */
 
-FsRez FsGetLastError(void)
+TinyFsRez FsGetLastError(void)
 {
 	printf("fs get last error");
 }
 
-FsRez fsInit(uint8_t *fsMem, size_t fsMemSize,
-             FsCb fsCb, FsH *fsH)
+TinyFsRez fsInit(uint8_t *fsMem, size_t fsMemSize,
+                 TinyFsCb fsCb, TinyFsH *fsH)
 {
 	if(fsMem == NULL)
 	{
-		return FS_FS_MEM_NULL_ERROR;
+		return TINY_FS_FS_MEM_NULL_ERROR;
 	}
 	if(fsMemSize == 0)
 	{
-		return FS_FS_MEM_SIZE_ERROR;
+		return TINY_FS_FS_MEM_SIZE_ERROR;
 	}
 	if(fsH == NULL)
 	{
-		return FS_FS_H_ERROR;
+		return TINY_FS_FS_H_ERROR;
 	}
-	if (fsCb.fsWriteBuff == NULL) {
-		return FS_CB_WRITE_BUFF_ERROR;
+	if (fsCb.writeBuff == NULL) {
+		return TINY_FS_CB_WRITE_BUFF_ERROR;
 	}
-    if (fsCb.fsReadBuff == NULL) {
-    	return FS_CB_READ_BUFF_ERROR;
+    if (fsCb.readBuff == NULL) {
+    	return TINY_FS_CB_READ_BUFF_ERROR;
     }
 
-    fsH->fsMem = fsMem;
-    fsH->fsMemSize = fsMemSize;
+    fsH->mem = fsMem;
+    fsH->memSize = fsMemSize;
     fsH->cb = fsCb;
 
     /*
---
+++ src/TinyFs.h
@@ -0,0 +1,60 @@
+﻿#ifndef __TINY_FS_H__
+#define __TINY_FS_H__
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+
+typedef enum {
+	TINY_FS_OK,
+	TINY_FS_BUFF_ERROR,
+	TINY_FS_FS_MEM_NULL_ERROR,
+	TINY_FS_FS_MEM_SIZE_ERROR,
+	TINY_FS_FS_H_ERROR,
+	TINY_FS_CB_WRITE_BUFF_ERROR,
+	TINY_FS_CB_READ_BUFF_ERROR,
+} TinyFsRez;
+
+typedef struct {
+ 	uint16_t readPos;
+ 	uint16_t writePos;
+ 	uint8_t firstSector;
+} TinyFsFile;
+
+typedef struct {
+    bool (*writeBuff)(size_t offset,
+                            uint8_t *data,
+                            size_t dataLen);
+    bool (*readBuff)(size_t offset,
+                           uint8_t *data,
+                           size_t dataLen);
+    bool (*lock)(bool lock);
+} TinyFsCb;
+
+typedef struct {
+    uint8_t *mem;
+    uint32_t memSize;
+    TinyFsCb cb;
+} TinyFsH;
+
+TinyFsRez tinyFsInit(uint8_t *fsMem, size_t fsMemSize,
+                     TinyFsCb FsCb, TinyFsH *TinyFsH);
+TinyFsRez tinyFsGetLastError(TinyFsH *TinyFsH);
+TinyFsRez tinyFsCreateFile(TinyFsH *TinyFsH, uint16_t fileName);
+TinyFsRez tinyFsOpenFile(TinyFsH *TinyFsH, uint16_t fileName, TinyFsFile *file);
+TinyFsRez tinyFsCloseFile(TinyFsH *TinyFsH, TinyFsFile *file);
+             
+/**
+ * @arg	[in,out] - input: target data size
+  output: the number of writed data
+ */
+TinyFsRez tinyFsWriteFile(TinyFsH *TinyFsH, TinyFsFile *file, uint8_t *data, size_t *dataSize);
+              
+/**
+ * @arg	[in,out] - input: target data size
+ *                 output: the number of read data
+ */
+TinyFsRez tinyFsReadFile(TinyFsH *TinyFsH, TinyFsFile *file, uint8_t *data, size_t *dataSize);
+ 
+#endif
